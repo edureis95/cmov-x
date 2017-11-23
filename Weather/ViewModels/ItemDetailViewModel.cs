3f6a4b30e0ef3e0d4a716bcc7b342d52b2be223d
ricardohtduarte <ricardohtduarte@gmail.com>
2017-11-23 14:51:31 +0000

    primeiro commit

---
+++ Weather/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Weather
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
